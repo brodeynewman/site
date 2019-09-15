424c149072e3078c5fb4ad50ec2b714982f181c6
Brodey Newman <brodeynewman@gmail.com>
2019-09-15 14:57:16 -0400

    site

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,16 @@
-import React from "react"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Oops..</h1>
+    <p>You just hit a page that doesn&#39;t exist. Sorry.</p>
+    <Link to="/">Home</Link>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,73 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+    <h1>Brodey Newman</h1>
+    <p>
+      I&apos;m a senior software engineer and business owner, <br />
+      currently working as an engineering manager at
+      <a
+        style={{
+          color: '#621FE8',
+          marginLeft: 5,
+          textDecoration: 'none',
+          fontWeight: 'bold',
+        }}
+        rel="noopener noreferrer"
+        target="_blank"
+        href="http://passportinc.com"
+      >
+        Passport.
+      </a>
+    </p>
+    <p>
+      I specialize in building distributed systems within the cloud, <br />
+      as well as performant and scalable user interfaces for the web.
+    </p>
+    <p>
+      Some of my favorite tools are:
+    </p>
+    <ul>
+      <li style={{ margin: 0 }}>React</li>
+      <li style={{ margin: 0 }}>AWS</li>
+      <li style={{ margin: 0 }}>Elixir</li>
+      <li style={{ margin: 0 }}>Node</li>
+      <li style={{ margin: 0 }}>Golang</li>
+    </ul>
+    <h3>About me</h3>
+    <p>
+      I currently live and work in Charlotte, North Carolina. <br />
+      When I&apos;m not writing code (which is rare),
+      you can find me dabbling in photography, <br />
+      running through the city,
+      reading at a coffee shop, or somewhere traveling.
 
-export default IndexPage
+    </p>
+    <h3>Get in touch</h3>
+    <a
+      style={{ color: '#621FE8', textDecoration: 'none' }}
+      href="mailto:brodeynewman@gmail.com"
+    >
+      brodeynewman@gmail.com
+    </a>
+    <a
+      style={{
+        color: '#621FE8',
+        textDecoration: 'none',
+        display: 'block',
+        marginTop: 10,
+      }}
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://github.com/brodeynewman"
+    >
+      GitHub
+    </a>
+  </Layout>
+);
+
+export default IndexPage;
